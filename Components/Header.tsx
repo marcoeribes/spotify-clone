044e32666e4c2f8d350f9167283bb7c2527f7e6f
marco <marcoeribes@gmail.com>
2023-07-06 23:23:42 -0700

    ADded Button and Header comps (need ListItem next)

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -3,6 +3,9 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight} from "react-icons/rx"
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -29,40 +32,116 @@ const Header: React.FC<HeaderProps> = ({
             className
         )}>
             <div className="
-                hidden
-                md:flex
-                gap-x-2
+                w-full
+                mb-4
+                flex
                 items-center
+                justify-between
             ">
-                <button 
-                    onClick={() => router.back()}
+                <div className="
+                    hidden
+                    md:flex
+                    gap-x-2
+                    items-center
+                ">
+                    <button 
+                        onClick={() => router.back()}
+                        className="
+                            rounded-full
+                            bg-black
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                        "
+                    >
+                        <RxCaretLeft className="text-white" size={35} /> 
+                    </button>
+                    <button 
+                        onClick={() => router.forward()}
+                        className="
+                            rounded-full
+                            bg-black
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                        "
+                    >
+                        <RxCaretRight className="text-white" size={35} /> 
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button
+                        className="
+                            rounded-full
+                            p-2
+                            bg-white
+                            flex
+                            items-center
+                            justify-center 
+                            hover:opacity-75
+                            transition
+                        "
+                    >
+                        <HiHome className="text-black" size={20}/>
+                    </button>
+                    <button
+                        className="
+                            rounded-full
+                            p-2
+                            bg-white
+                            flex
+                            items-center
+                            justify-center 
+                            hover:opacity-75
+                            transition
+                        "
+                    >
+                        <BiSearch className="text-black" size={20}/>
+                    </button>
+                </div>
+                <div
                     className="
-                        rounded-full
-                        bg-black
                         flex
+                        justify-between
                         items-center
-                        justify-center
-                        hover:opacity-75
-                        transition
+                        gap-x-4
                     "
                 >
-                    <RxCaretLeft className="text-white" size={35} /> 
-                </button>
-                <button 
-                    onClick={() => router.forward()}
-                    className="
-                        rounded-full
-                        bg-black
-                        flex
-                        items-center
-                        justify-center
-                        hover:opacity-75
-                        transition
-                    "
-                >
-                    <RxCaretRight className="text-white" size={35} /> 
-                </button>
+                    <>
+                        <div>
+                            <Button
+                                onClick={() => {}} 
+                                className="
+                                    bg-transparent
+                                    text-neutral-300
+                                    font-medium
+                                "
+                            >
+                                Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                                onClick={() => {}}
+                                className="
+                                    bg-white
+                                    px-6
+                                    py-2
+                                "
+                            >
+                                Login
+                            </Button>
+                        </div>
+                    </>
+                </div>
             </div>
+            {children}
+
+
         </div>
     )
 }
